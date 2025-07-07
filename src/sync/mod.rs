5bb1e8832c1b3a98cb8989b08ad2c3567d45399d
jmhrpr <25673452+jmhrpr@users.noreply.github.com>
2025-07-07 13:33:32 +0100

    rename testnet to testnet4

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -45,7 +45,7 @@ pub struct IndexersConfig {
 #[serde(rename_all = "lowercase")]
 pub enum Network {
     Mainnet,
-    Testnet,
+    Testnet4,
 }
 
 impl Network {
@@ -53,7 +53,7 @@ impl Network {
     pub fn genesis_block(&self) -> Block {
         match self {
             Self::Mainnet => bitcoin::constants::genesis_block(bitcoin::Network::Bitcoin),
-            Self::Testnet => {
+            Self::Testnet4 => {
                 let raw_block = hex::decode("0100000000000000000000000000000000000000000000000000000000000000000000004e7b2b9128fe0291db0693af2ae418b767e657cd407e80cb1434221eaea7a07a046f3566ffff001dbb0c78170101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff5504ffff001d01044c4c30332f4d61792f323032342030303030303030303030303030303030303030303165626435386332343439373062336161396437383362623030313031316662653865613865393865303065ffffffff0100f2052a010000002321000000000000000000000000000000000000000000000000000000000000000000ac00000000").unwrap();
                 Block::consensus_decode_from_finite_reader(&mut &raw_block[..]).unwrap()
             }
@@ -63,7 +63,7 @@ impl Network {
     pub fn magic(&self) -> Magic {
         match self {
             Self::Mainnet => bitcoin::Network::Bitcoin.magic(),
-            Self::Testnet => Magic::from_bytes([0x1c, 0x16, 0x3f, 0x28]),
+            Self::Testnet4 => Magic::from_bytes([0x1c, 0x16, 0x3f, 0x28]),
         }
     }
 }

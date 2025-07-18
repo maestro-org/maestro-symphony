1bd9a716d0d4b5946dddda32a5fe91fa28b8ce2e
jmhrpr <25673452+jmhrpr@users.noreply.github.com>
2025-07-18 13:01:14 +0100

    refactor: cleaner logs

--- src/main.rs
+++ src/main.rs
@@ -78,6 +78,8 @@ async fn main() -> Result<(), ()> {
     let subscriber = tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .with_writer(std::io::stdout)
+        .with_target(false)
+        .with_ansi(false)
         .finish();
 
     tracing::subscriber::set_global_default(subscriber)

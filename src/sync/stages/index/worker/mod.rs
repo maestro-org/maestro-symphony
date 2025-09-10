41558bfaf4db922e63a40257f0836c38fd3bf6ea
jmhrpr <25673452+jmhrpr@users.noreply.github.com>
2025-09-10 21:54:35 +0100

    tweak ratios

--- src/sync/stages/index/worker/mod.rs
+++ src/sync/stages/index/worker/mod.rs
@@ -14,10 +14,10 @@ fn get_default_cache_size() -> u64 {
         .map(|x| x.total_memory)
         .unwrap_or_else(|| system.total_memory());
 
-    let default_budget = (total_memory as f64 * 0.2) as u64; // 20% of total memory
+    let default_budget = (total_memory as f64 * 0.25) as u64;
 
     info!(
-        "No UTxO cache memory budget specified, using 20% of system memory: {:.2} GB ({} bytes) out of {:.2} GB total",
+        "No UTxO cache memory budget specified, using 25% of system memory: {:.2} GB ({} bytes) out of {:.2} GB total",
         default_budget as f64 / (1024.0 * 1024.0 * 1024.0),
         default_budget,
         total_memory as f64 / (1024.0 * 1024.0 * 1024.0)
